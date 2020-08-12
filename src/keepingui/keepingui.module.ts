48189312361d933d0539ef05834e8611ac3fefee
laplaceliu <laplaceliu@outlook.com>
2020-08-12 15:38:41 +0800

    boilerplate

---
+++ src/keepingui/keepingui.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { KeepinguiComponent } from './keepingui.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [KeepinguiComponent]
+})
+export class KeepinguiModule { }
